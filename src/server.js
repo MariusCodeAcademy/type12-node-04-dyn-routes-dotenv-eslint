df6f0773933285332f8982af5db5a5b4eab1064f
Marius <marius@example.com>
2022-08-25 09:26:12 +0300

    uzd

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 'use strict';
-console.log('server.js');
 
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const colors = require('colors');
 
 const app = express();
 const PORT = 3000;
+
+// DATA
+const users = [
+  { id: 3, name: 'James', age: 20 },
+  { id: 1, name: 'Serbentautas', age: 45 },
+  { id: 2, name: 'Lenteja', age: 30 },
+];
 
 // MIddleware
 app.use(morgan('dev'));
@@ -16,4 +23,12 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+// GET /api/users - grazina visus userius
+
+// GET /api/users/2 - grazina useri kurio id yra 2 (ne pagal indexa)
+
+// GET /api/users/age/30 - grazina useri kurio amzius yra 30 (ne pagal indexa)
+
+// GET /api/users/age/lt/33 - grazina userius kuriu amzius maziau nei 33 (ne pagal indexa)
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`.cyan.bold));
