ff0db7f46f563fb4b95653242d20943bfb8c4ad8
Marius <marius@example.com>
2022-08-25 09:31:12 +0300

    GET /api/users

--- src/server.js
+++ src/server.js
@@ -24,11 +24,18 @@ app.get('/', (req, res) => {
 });
 
 // GET /api/users - grazina visus userius
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
 
 // GET /api/users/2 - grazina useri kurio id yra 2 (ne pagal indexa)
 
 // GET /api/users/age/30 - grazina useri kurio amzius yra 30 (ne pagal indexa)
 
 // GET /api/users/age/lt/33 - grazina userius kuriu amzius maziau nei 33 (ne pagal indexa)
+
+app.use((req, res) => {
+  res.status(404).json({ msg: 'Not found' });
+});
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`.cyan.bold));
